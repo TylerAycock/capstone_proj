308a2071e8713493708da74e4017797013a82f60
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-07-05 11:16:17 -0700

    added date function

--- public/parks.js
+++ public/parks.js
@@ -1,54 +1,78 @@
-const form = document.querySelector('form')
-const parkSelect = document.getElementById('park-select')
-const search = document.getElementById('submit')
-const parkTable = document.getElementById('park-table')
+const form = document.querySelector("form");
+const parkSelect = document.getElementById("park-select");
+const search = document.getElementById("submit");
+const parkTable = document.getElementById("park-table");
 
-let natParks = []
+let natParks = [];
 
-const getParks = ()=>{
-    parkSelect.innerHTML=''
+const getParks = () => {
+  parkSelect.innerHTML = "";
 
-    axios.get(`/api/parks`)
-    .then((resp)=>{
-        natParks = [...resp.data]
-       resp.data.forEach(park =>{
-        const option = document.createElement('option')
-        option.setAttribute('value', park.park_id)
-        option.textContent = park.park_name
-        parkSelect.appendChild(option)
-       })
-    }).catch(err=>console.log(err))
-}
-
-const tableData = (arr) =>{
-    parkTable.innerHTML=""
-    arr.forEach(park =>{
-        const row = document.createElement('tr')
-        const name = document.createElement('td')
-        const state = document.createElement('td')
-        name.textContent = park.park_name
-        state.textContent = park.park_id
-        row.appendChild(name)
-        row.appendChild(state)
-        parkTable.appendChild(row)
-})}
-
-const getTable = ()=>{
-    console.log('acquiring park list')
-    axios.get(`/api/table`)
-    .then(resp=>{
-        tableData(resp.data)
+  axios
+    .get(`/api/parks`)
+    .then((resp) => {
+      natParks = [...resp.data];
+      resp.data.forEach((park) => {
+        const option = document.createElement("option");
+        option.setAttribute("value", park.park_id);
+        option.textContent = park.park_name;
+        parkSelect.appendChild(option);
+      });
     })
-    .catch(err=>console.log(err))
+    .catch((err) => console.log(err));
+};
+
+const tableData = (arr) => {
+  parkTable.innerHTML = "";
+  arr.forEach((park) => {
+    let{park_id, park_name,state,acres, est} = park
+    const row = document.createElement("tr");
+    const name = document.createElement("td");
+    const parkState = document.createElement("td");
+    const parkAcres = document.createElement("td")
+    const established = document.createElement("td")
+    name.textContent = park_name;
+    parkState.textContent = state;
+    parkAcres.textContent = acres;
+    established.textContent = converted(est);
+    row.appendChild(name);
+    row.appendChild(parkState);
+    row.appendChild(parkAcres);
+    row.appendChild(established);
+    parkTable.appendChild(row);
+  });
+};
+
+const getTable = () => {
+  console.log("acquiring park list");
+  axios
+    .get(`/api/table`)
+    .then((resp) => {
+      tableData(resp.data);
+    })
+    .catch((err) => console.log(err));
+};
+
+const filterParks = (evt) => {
+  console.log(parkSelect.value);
+  console.log(natParks);
+  let parksToDisplay = natParks.filter(
+    (obj) => +obj.park_id === +parkSelect.value
+  );
+  tableData(parksToDisplay);
+};
+
+let monthWords = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+const converted = (date) =>{
+  dateParts= date.split("-")
+  year = dateParts[0]
+  month = monthWords[+dateParts[1]-1]
+  day = dateParts[2]
+  return `${month} ${day}, ${year}`
 }
 
-const filterParks = evt=>{
-    console.log(parkSelect.value)
-    console.log(natParks)
-    let parksToDisplay = natParks.filter((obj)=> +obj.park_id === +parkSelect.value)
-    tableData(parksToDisplay)
-}
-getParks()
-getTable()
+getParks();
+getTable();
 
-search.addEventListener('click', filterParks)
+search.addEventListener("click", filterParks);
